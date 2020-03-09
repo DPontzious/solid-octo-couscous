6b67a1b9216da2193a9c3bdb82a4ff742230afec
Danelle Pontzious <danellepontzious@Danelles-Air.domain>
2020-03-09 11:12:51 -0700

    added data binding

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/userSettings'
 
 @Component({
   selector: 'app-user-settings-form',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
+  orginalUserSettings: UserSettings = {
+    name: "Danelle",
+    emailOffers: true,
+    interfaceStyle: "dark",
+    subscriptionType: "Annual",
+    notes: "no notes"
+  };
+
+  userSettings: UserSettings = { ...this.orginalUserSettings }
 
   constructor() { }
 
